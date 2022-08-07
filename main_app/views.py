463125fbad3c8bba70f9de7783ae20dcc6b85448
Jonesdy99 <jonesdylan1165@gmail.com>
2022-08-07 16:08:27 -0400

    implement task form html

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 from .models import Task
 
+# ReverseLazy
 # Create your views here.
 class TaskList(ListView):
   model = Task
@@ -11,4 +14,10 @@ class TaskList(ListView):
 class TaskDetail(DetailView):
   model = Task
   context_object_name = 'task'
+
+
+class TaskCreate(CreateView):
+  model = Task
+  fields = '__all__'
+  success_url = reverse_lazy('tasks') 
 
