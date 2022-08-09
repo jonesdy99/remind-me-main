a8be363a7076d76f8191a9d91adbc46e9ca4e4c1
Jonesdy99 <jonesdylan1165@gmail.com>
2022-08-09 10:53:17 -0400

    implement new time feature

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+import datetime
 
 # Create your models here.
 
@@ -8,7 +9,7 @@ class Task(models.Model):
   title = models.CharField(max_length=200)
   description = models.TextField(null=True, blank=True)
   complete = models.BooleanField(default=False)
-  created = models.DateTimeField(auto_now_add=True)
+  created = models.DateField(("Date"), default=datetime.date.today)
 
   def __str__(self):
     return self.title
